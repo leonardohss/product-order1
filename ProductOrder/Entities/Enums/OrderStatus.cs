d24f1c24cbc01a6c3da9201258d435992255a0bd
Leonardo Sousa <leorick97@gmail.com>
2019-06-05 22:19:33 -0300

    Create the project and Implementation of Order Class and OrderStatus Enum

---
+++ ProductOrder/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductOrder.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        pending_payment = 0,
+        processing = 1,
+        shipped = 2,
+        deliveres = 3
+    }
+}
---
+++ ProductOrder/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProductOrder.Entities.Enums;
+
+namespace ProductOrder.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status = new OrderStatus();
+
+    }
+}
